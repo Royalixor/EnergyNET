38dbbf64b020a0d554da685e6bf5df14444502d1
Royalixor <suitedslime@btinternet.com>
2013-09-13 01:43:24 +0100

    Back to basics

---
+++ common/com/royalixor/enet/core/CommonProxxy.java
@@ -0,0 +1,19 @@
+package com.royalixor.enet.core;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import cpw.mods.fml.common.network.IGuiHandler;
+
+public class CommonProxxy implements IGuiHandler {
+
+	@Override
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+	    return null;
+    }
+
+	@Override
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+	    return null;
+    }
+
+}
---
+++ common/com/royalixor/enet/core/util/ModInfo.java
@@ -0,0 +1,26 @@
+package com.royalixor.enet.core.util;
+
+public class ModInfo {
+	
+	// General variable definitions
+	public static final String CHANNEL = "ENET";
+	public static final String ID = "ENET";
+	public static final String NAME = "EnergyNET";
+	public static final String DOMAIN = "enet";
+	public static final String PREFIX = DOMAIN + ":";
+	public static final String MAJOR_VERSION = "@MAJOR@";
+	public static final String MINOR_VERSION = "@MINOR@";
+	public static final String REVISION_VERSION = "@REVIS@";
+	public static final String VERSION = MAJOR_VERSION + "." + MINOR_VERSION + "." + REVISION_VERSION;
+	public static final String BUILD_VERSION = "@BUILD@";
+	
+	// Directories
+	public static final String RESOURCE_DIR = "/assets/enet/";
+	public static final String LANGUAGE_DIR = RESOURCE_DIR + "lang/";
+	public static final String TEXTURE_DIR = "textures/";
+	public static final String BLOCK_DIR = TEXTURE_DIR + "blocks/";
+	public static final String ITEM_DIR = TEXTURE_DIR + "items/";
+	public static final String MODEL_DIR = TEXTURE_DIR + "models/";
+	public static final String GUI_DIR = TEXTURE_DIR + "gui/";
+
+}
